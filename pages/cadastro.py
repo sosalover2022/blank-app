328d36a7cea10389e69567e62b089cd3184c8465
sosalover2022 <joaofelipe4557@gmail.com>
2025-07-21 17:47:56 +0000

    ;

---
+++ pages/cadastro.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+nome = st.text_input("Digite seu nome:")
+st.write("Você digitou:", nome)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Music RecC")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
